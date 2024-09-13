669371b3101d5d38da790f91784a4aace530ad38
Carlos Poon <carlos.poon@broadcom.com>
2024-09-13 17:08:34 -0400

    WIP - Adding Question and Answer to WebSocketController
    Co-authored-by: Satish Gururajan <satish-prahalad.gururajan@broadcom.com>

--- apps/acme-assist/src/main/java/com/example/acme/assist/FitAssistWebSocketController.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/FitAssistWebSocketController.java
@@ -1,21 +1,33 @@
 package com.example.acme.assist;
 
-import com.example.acme.assist.model.Greeting;
-import com.example.acme.assist.model.HelloMessage;
+import com.example.acme.assist.model.AcmeChatRequest;
+import com.example.acme.assist.model.AcmeChatResponse;
+import com.example.acme.assist.model.GreetingRequest;
+import com.example.acme.assist.model.GreetingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class FitAssistWebSocketController {
 
+    @Autowired
+    private FitAssistChatService fitAssistChatService;
+
+
     @MessageMapping("/hello")
     @SendTo("/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        System.out.println("Hello received.");
-        Thread.sleep(1000); // simulated delay
-        return Greeting.builder().content("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!").build();
+    public GreetingResponse greeting(GreetingRequest request) throws Exception {
+        System.out.println("Hello received - " + request);
+        return fitAssistChatService.getGreetingResponse(request);
+    }
+
+    @MessageMapping("/question")
+    @SendTo("/answer")
+    public AcmeChatResponse question(AcmeChatRequest request) throws Exception {
+       System.out.println("Question received::" + request);
+       return fitAssistChatService.getChatResponse(request);
     }
 
 }
--- apps/acme-assist/src/main/resources/static/app.js
+++ apps/acme-assist/src/main/resources/static/app.js
@@ -6,7 +6,12 @@ stompClient.onConnect = (frame) => {
     setConnected(true);
     console.log('Connected: ' + frame);
     stompClient.subscribe('/greetings', (greeting) => {
-        showGreeting(JSON.parse(greeting.body).content);
+        console.log('Greeting' + greeting);
+        showGreeting(JSON.parse(greeting.body).suggestedPrompts[0]);
+    });
+    stompClient.subscribe('/answer', (chatResponse) => {
+        console.log('Answer' + chatResponse);
+        showGreeting(JSON.parse(chatResponse.body).messages[0])
     });
 };
 
@@ -24,8 +29,7 @@ function setConnected(connected) {
     $("#disconnect").prop("disabled", !connected);
     if (connected) {
         $("#conversation").show();
-    }
-    else {
+    } else {
         $("#conversation").hide();
     }
     $("#greetings").html("");
@@ -44,8 +48,27 @@ function disconnect() {
 function sendName() {
     stompClient.publish({
         destination: "/hello",
-        body: JSON.stringify({'name': $("#name").val()})
+        // body: JSON.stringify({'name': $("#name").val()})
+        body: JSON.stringify(
+            {
+                conversationId: 'test-id',
+                userId: $("#name").val(),
+                page: '/'
+            })
     });
+}
+
+function sendQuestion() {
+    stompClient.publish({
+        destination: "/question",
+        body: JSON.stringify(
+            {
+                page:'/question',
+                productId: '42ca484d-1e2a-4a3c-bc65-556df7d8f3cd',
+                messages: [{role:'USER', content:$("#question").val(), currentProduct: ''}]
+            }
+        )
+    })
 }
 
 function showGreeting(message) {
@@ -54,7 +77,8 @@ function showGreeting(message) {
 
 $(function () {
     $("form").on('submit', (e) => e.preventDefault());
-    $( "#connect" ).click(() => connect());
-    $( "#disconnect" ).click(() => disconnect());
-    $( "#send" ).click(() => sendName());
+    $("#connect").click(() => connect());
+    $("#disconnect").click(() => disconnect());
+    $("#send").click(() => sendName());
+    $("#send-question").click(() => sendQuestion());
 });
