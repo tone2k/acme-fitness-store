9ec3822181410680de93a9ca727a115d4b417691
Will Sather <satherw@vmware.com>
2024-09-18 16:43:14 -0500

    chore: improve credit card icons

--- apps/acme-shopping-react/src/AcmeFooter.tsx
+++ apps/acme-shopping-react/src/AcmeFooter.tsx
@@ -1,4 +1,4 @@
-import {Box, createTheme, Divider, Grid, Link, Stack, TextField, ThemeProvider, Typography} from "@mui/material";
+import {createTheme, Divider, Grid, Link, Stack, TextField, ThemeProvider, Typography} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import XIcon from '@mui/icons-material/X';
@@ -48,16 +48,16 @@ export default function AcmeFooter() {
                                 <Typography variant='h6' marginBottom={1}>Pages</Typography>
 
                                 {/*TODO: fix these links that don't go anywhere*/}
-                                <Link href="/" underline="hover" sx={{ color: "black" }}>About us</Link>
-                                <Link href="/" underline="hover" sx={{ color: "black" }}>Terms and Conditions</Link>
-                                <Link href="/" underline="hover" sx={{ color: "black" }}>FAQ</Link>
-                                <Link href="/contact" underline="hover" sx={{ color: "black" }}>Contact us</Link>
+                                <Link href="/" underline="hover" sx={{color: "black"}}>About us</Link>
+                                <Link href="/" underline="hover" sx={{color: "black"}}>Terms and Conditions</Link>
+                                <Link href="/" underline="hover" sx={{color: "black"}}>FAQ</Link>
+                                <Link href="/contact" underline="hover" sx={{color: "black"}}>Contact us</Link>
                             </Stack>
 
                             <Divider sx={{width: "100%", marginY: 2}}/>
 
                             <Typography variant='h6' marginBottom={1}>User</Typography>
-                            <Link href="/acme-login" underline="hover" sx={{ color: "black" }}>Login</Link>
+                            <Link href="/acme-login" underline="hover" sx={{color: "black"}}>Login</Link>
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={4} paddingX={{xs: 0, sm: 2}}>
@@ -70,15 +70,16 @@ export default function AcmeFooter() {
                         </Grid>
 
                         <Grid item xs={12} sm={6} md={4} paddingX={{xs: 0, sm: 2}}>
-                            <Divider sx={{ width: 1, marginY: 2, display: {xs: 'flex', md: 'none'}}}/>
+                            <Divider sx={{width: 1, marginY: 2, display: {xs: 'flex', md: 'none'}}}/>
 
-                            <Typography variant='h6' sx={{ mb: 1 }}>Get the news</Typography>
-                            <Typography sx={{ mb: 2 }}>What's new in the world of Fitness</Typography>
-                            <TextField sx={{ width: '100%' }} size='small' placeholder="Enter your email." InputProps={{ endAdornment: <SendIcon/>}} />
+                            <Typography variant='h6' sx={{mb: 1}}>Get the news</Typography>
+                            <Typography sx={{mb: 2}}>What's new in the world of Fitness</Typography>
+                            <TextField sx={{width: '100%'}} size='small' placeholder="Enter your email."
+                                       InputProps={{endAdornment: <SendIcon/>}}/>
 
                             <Divider sx={{width: "100%", my: 2}}/>
 
-                            <Typography variant='h6' sx={{ mt: 3, mb: 2 }}>Stay in touch</Typography>
+                            <Typography variant='h6' sx={{mt: 3, mb: 2}}>Stay in touch</Typography>
                             <Stack direction="row" spacing={2}>
                                 <FacebookIcon/>
                                 <XIcon/>
@@ -88,25 +89,24 @@ export default function AcmeFooter() {
                     </Grid>
                 </Container>
 
-                <Box sx={{background: "#333", paddingY: 4}}>
-                    <Container maxWidth="xl">
-                        <Grid container direction='row' paddingX={{xs: 0, sm: 2}}>
-                            <Grid item xs={12} sm={6} sx={{textAlign: {xs: 'center', sm: 'left'}}}>
-                                <Typography variant="body2">{`© ${new Date().getFullYear()} ACME Fitness`}</Typography>
-                            </Grid>
+                <Stack direction={{xs: 'column', md: 'row'}} alignItems='center' justifyContent="space-between" sx={{background: "#333", p: 4 }}>
+                    <span>
+                        <Typography variant="body2">{`© ${new Date().getFullYear()} ACME Fitness`}</Typography>
+                        <Typography variant="body2" sx={{fontStyle: 'italic'}}>
+                            This website is for demo purposes only. It is not an actual e-commerce site.
+                        </Typography>
+                    </span>
 
-                            <Grid item xs={12} sm={6} sx={{textAlign: {xs: 'center', sm: 'right'}, my: { xs: 1, sm: 0}}} >
-                                <img src="/payment.png" alt="payment-options"/>
-                            </Grid>
+                    <Stack direction='row' justifyContent='end' alignItems='center' gap={1} sx={{ mt: {xs: 4, md: 0 } }}>
+                        <img src="/payment/visa.png" width={50} alt="visa"/>
+                        <img src="/payment/discover.png" width={50} alt="discover"/>
+                        <img src="/payment/mastercard.png" width={50} alt="mastercard"/>
+                        <img src="/payment/american-express.png" width={50} alt="american-express"/>
+                        <img src="/payment/paypal.png" width={50} alt="paypal"/>
+                    </Stack>
 
-                            <Grid item xs={12} sx={{textAlign: {xs: 'center', sm: 'left'}}}>
-                                <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
-                                    This website is for demo purposes only. It is not an actual e-commerce site.
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Container>
-                </Box>
+
+                </Stack>
             </Stack>
         </ThemeProvider>
     );
