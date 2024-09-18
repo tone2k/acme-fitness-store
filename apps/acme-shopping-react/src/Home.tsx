08d162cfa7b793e606a006dbe85a87aa302f4571
Will Sather <satherw@vmware.com>
2024-09-18 16:16:58 -0500

    chore: improve home and contact styling

--- apps/acme-shopping-react/src/Home.tsx
+++ apps/acme-shopping-react/src/Home.tsx
@@ -1,4 +1,4 @@
-import {Box, Divider, Grid, Stack, Typography} from "@mui/material";
+import {Divider, Stack, Typography} from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/FavoriteBorder';
 import SellIcon from '@mui/icons-material/Sell';
 import SendIcon from '@mui/icons-material/Send';
@@ -11,64 +11,69 @@ import CatalogCarousel from "./CatalogCarousel.tsx";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Container from "@mui/material/Container";
 
 export default function Home() {
 
     const info = [
-        {text1: "Satisfied", text2: "Customers", image: <FavoriteIcon fontSize='large'/>},
-        {text1: "Best", text2: "Prices", image: <SellIcon fontSize='large'/>},
-        {text1: "Next Day", text2: "Delivery", image: <SendIcon fontSize='large'/>},
-        {text1: "Free Returns", text2: "for 3 Months", image: <LoopIcon fontSize='large'/>},
+        {text: "Satisfied Customers", image: <FavoriteIcon fontSize='large'/>},
+        {text: "Best Prices", image: <SellIcon fontSize='large'/>},
+        {text: "Next Day Delivery", image: <SendIcon fontSize='large'/>},
+        {text: "Free Returns for 3 Months", image: <LoopIcon fontSize='large'/>},
     ]
 
     return (
-        <Container maxWidth="xl">
-            <Stack justifyContent='center' alignItems='center' spacing={5} marginBottom={4}>
-                <CatalogCarousel/>
-                <Divider sx={{width: "50%"}}></Divider>
+        <Stack
+            justifyContent='center'
+            alignItems='center'
+            spacing={5}
+            marginBottom={4}
+            sx={{mx: {xs: 3, sm: 25}}}
+        >
+            <CatalogCarousel/>
+
+            <Divider sx={{width: "50%"}}></Divider>
+
+            <Stack alignItems='center'>
                 <Typography
                     variant='h5'
-                    sx={{fontSize: '1.5rem', fontWeight: '1000', letterSpacing: '0.18rem'}}
+                    sx={{my: 2, fontWeight: '1000', letterSpacing: '0.18rem', textAlign: 'center'}}
                 >
                     ABOUT ACME FITNESS
                 </Typography>
-                <Box sx={{paddingLeft: {xs: 2, sm: 4}, maxWidth: {xs: '350px', sm: '700px'}}}>
-                    <Grid container direction='row' spacing={{xs: 2, sm: 10}} justifyContent='center' alignItems='center'>
-                        {info.map((item, index) => (
-                            <Grid item xs={6} sm={3} lg={3} key={`info-${index}`}>
-                                <Stack direction='row' alignItems='center' spacing={3}>
-                                    {item.image}
-                                    <Stack>
-                                        <Typography>{item.text1}</Typography>
-                                        <Typography>{item.text2}</Typography>
-                                    </Stack>
-                                </Stack>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-                <Divider sx={{width: "50%"}}></Divider>
-                <Button
-                    color='inherit'
-                    key='catalog'
-                    component={RouterLink}
-                    to='catalog'
-                    sx={{fontSize: '1.5rem', fontWeight: '1000', letterSpacing: '0.18rem'}}
-                >
-                    New Arrivals
-                </Button>
-                <span>
-                        <Typography display="inline">Free shipping </Typography>
-                        <Typography display="inline" sx={{color: 'secondary.contrastText'}}>on all</Typography>
-                    </span>
-                <Stack alignItems='center' sx={{backgroundColor: 'transparent', textAlign: 'center'}} spacing={2}>
-                    <TravelExploreIcon fontSize='large'/>
-                    <Typography>Do you want to explore more?</Typography>
-                    <Typography>We keep updating our inventory with exciting new products !!</Typography>
-                    <Button variant='outlined' color='inherit' component={RouterLink} to='catalog'>Find More in Catalog</Button>
+
+                <Stack direction={{ xs: 'column', sm: 'row' }} justifyContent='center' alignItems='center' gap={2}>
+                    {info.map((item) => (
+                        <Stack direction='row' alignItems='center' spacing={3} sx={{ width: 1 }}>
+                            {item.image}
+                            <Typography sx={{ flexGrow: 1 }}>{item.text}</Typography>
+                        </Stack>
+                    ))}
                 </Stack>
             </Stack>
-        </Container>
+
+
+            <Divider sx={{width: "50%"}}/>
+
+            <Button
+                color='inherit'
+                key='catalog'
+                component={RouterLink}
+                to='catalog'
+                sx={{fontSize: '1.5rem', fontWeight: '1000', letterSpacing: '0.18rem'}}
+            >
+                New Arrivals
+            </Button>
+
+            <Typography>Free shipping on all</Typography>
+
+            <Stack alignItems='center' sx={{backgroundColor: 'transparent', textAlign: 'center'}} spacing={2}>
+                <TravelExploreIcon fontSize='large'/>
+                <Typography>Do you want to explore more?</Typography>
+                <Typography>We keep updating our inventory with exciting new products !!</Typography>
+                <Button variant='outlined' color='inherit' component={RouterLink} to='catalog'>
+                    Find More in Catalog
+                </Button>
+            </Stack>
+        </Stack>
     );
 }
