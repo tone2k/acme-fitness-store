81ab4258d8692c49f6fa96bf9e7bc455b8ca9f2e
Will Sather <satherw@vmware.com>
2024-10-10 16:03:43 -0400

    fix: product card image fit

--- apps/acme-shopping-react/src/accessories/page.tsx
+++ apps/acme-shopping-react/src/accessories/page.tsx
@@ -14,9 +14,9 @@ export default function AccessoriesPage() {
     return <Error />;
   }
 
-  const accessories = data.data.filter(
-    (product) => !product.tags.includes("bicycle")
-  );
+  const accessories = data.data
+    .filter((product) => !product.tags.includes("bicycle"))
+    .reverse();
 
   if (accessories.length <= 0) {
     return (
@@ -46,7 +46,7 @@ export default function AccessoriesPage() {
 
       <hr className="border-8 border-mint border-dashed border-spacing-8" />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 bg-navy-50 p-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 bg-navy-50 p-8 md:p-16">
         {accessories.map((accessory) => (
           <ProductCard key={accessory.id} product={accessory} />
         ))}
--- apps/acme-shopping-react/src/components/ProductCard.tsx
+++ apps/acme-shopping-react/src/components/ProductCard.tsx
@@ -4,7 +4,7 @@ export default function ProductCard({ product }: { product: ProductData }) {
   return (
     <a href={`/product/${product.id}`} className="w-full">
       <img
-        className="rounded-lg object-cover h-[28rem] w-full"
+        className="rounded-lg object-contain h-[28rem] w-full bg-white"
         src={product.imageUrl1}
         alt={product.id}
       />
