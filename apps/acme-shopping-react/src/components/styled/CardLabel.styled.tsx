77852bce8e1c9db18b793b20c04fdc7982ea028c
Will Sather <satherw@vmware.com>
2024-10-14 17:15:24 -0400

    chore: remove unused styled components

--- apps/acme-shopping-react/src/components/styled/CardLabel.styled.tsx
+++
@@ -1,9 +0,0 @@
-import {styled, Typography} from "@mui/material";
-
-export const CardLabelStyled = styled(Typography)(({ theme }) => ({
-    fontFamily: 'Roboto, sans-serif',
-    fontSize: '1.25em',
-    fontWeight: 600,
-    color: '#140A00',
-    lineHeight: '150%',
-}));
